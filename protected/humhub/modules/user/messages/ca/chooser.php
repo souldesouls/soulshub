99c575be4f938d6f8d7699114a992150c81b05ae
Simon McCorkindale <simon@funx.jp>
2021-04-21 22:07:31 +0800

    Run php protected/yii message/extract-module user after downgrading to php 7.2

--- protected/humhub/modules/user/messages/ca/chooser.php
+++ protected/humhub/modules/user/messages/ca/chooser.php
@@ -1,14 +1,8 @@
 <?php
-return array (
-  'Add more...' => 'Afegeix més...',
-  'An unexpected error occurred while loading the result.' => 'S\'ha produït un error inesperat en carregar el resultat.',
-  'Load more' => 'Carregar més',
-  'No users found for the given query.' => 'No s\'ha trobat cap usuari per aquesta consulta.',
-  'Please enter at least {n} character' => '',
-  'Select {n,plural,=1{item} other{items}}' => '',
-  'Select {n,plural,=1{user} other{users}}' => '',
-  'This field only allows a maximum of {n,plural,=1{# item} other{# items}}.' => '',
-  'This field only allows a maximum of {n,plural,=1{# user} other{# users}}.' => '',
-  'You reached the maximum number of allowed characters ({n}).' => '',
-  'Your search returned no matches.' => 'La teva cerca no ha donat cap resultat.',
-);
+
+return [
+    'Add more...' => 'Afegeix més...',
+    'No users found for the given query.' => 'No s\'ha trobat cap usuari per aquesta consulta.',
+    'Select {n,plural,=1{user} other{users}}' => '',
+    'This field only allows a maximum of {n,plural,=1{# user} other{# users}}.' => '',
+];
--- protected/humhub/modules/user/messages/ru/chooser.php
+++ protected/humhub/modules/user/messages/ru/chooser.php
@@ -1,18 +1,8 @@
 <?php
-/**
- * Translation: Paul (https://paul.bid) paulbid@protonmail.com
- * 
- */
-return array (
-  'Add more...' => 'Добавить больше...',
-  'An unexpected error occurred while loading the result.' => 'Возникла неожиданная ошибка при загрузке результата.',
-  'Load more' => 'Показать ещё',
-  'No users found for the given query.' => 'Пользователей по данному запросу не найдено.',
-  'Please enter at least {n} character' => 'Пожалуйста, введите не менее {n} символов',
-  'Select {n,plural,=1{item} other{items}}' => 'Выберите {n,plural,=1{элемент} other{элементы}}',
-  'Select {n,plural,=1{user} other{users}}' => 'Выберите {n,plural,=1{пользователя} other{пользователей}}',
-  'This field only allows a maximum of {n,plural,=1{# item} other{# items}}.' => 'Это поле поддерживает максимум {n,plural,=1{элемент} other{элементов}}',
-  'This field only allows a maximum of {n,plural,=1{# user} other{# users}}.' => 'Это поле поддерживает максимум {n,plural,=1{пользователя} other{пользователей}}',
-  'You reached the maximum number of allowed characters ({n}).' => 'Вы достигли максимально допустимого числа символов ({n})',
-  'Your search returned no matches.' => 'Ваш поиск не дал результатов.',
-);
+
+return [
+    'Add more...' => 'Добавить больше...',
+    'No users found for the given query.' => 'Пользователей по данному запросу не найдено.',
+    'Select {n,plural,=1{user} other{users}}' => 'Выберите {n,plural,=1{пользователя} other{пользователей}}',
+    'This field only allows a maximum of {n,plural,=1{# user} other{# users}}.' => 'Это поле поддерживает максимум {n,plural,=1{пользователя} other{пользователей}}',
+];
